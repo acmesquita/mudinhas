11fc8b8113b2793a193d9bb0c5b21fc37edf4c86
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-01-20 23:29:17 -0300

    style: adicionando fontes do sistema

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,9 @@ export default function Login() {
   return (
     <>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Mudinhas</title>
       </Head>
-
       <main className={styles.container}>
         <h1>Bem vindos</h1>
         <h2>Mudinhas</h2>
